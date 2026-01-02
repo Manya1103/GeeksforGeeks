cb6957a158e29bd7ab3ffc2091418b99a53cde23
Manya Garg <manya111203@gmail.com>
2026-01-02 09:40:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.cpp
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.cpp
@@ -5,30 +5,47 @@ class Solution {
         // two-pass algorithm
  // Sorts the array by counting 0s, 1s, and 2s and then rewriting them in order.
         int n = arr.size();
-        int i;
-        int c0s = 0;
-        int c1s = 0;
-        int c2s = 0;
-        for (i=0; i<n; i++){
-        if (arr[i]==0){
-            c0s+=1;
-        }else if (arr[i]==1){
-            c1s+=1;
-        }else if (arr[i]==2){
-            c2s+=1;
-        }
-        }
-        int idx=0;
-        for (i=0; i<c0s; i++){
-            arr[idx++] = 0;
+        // int i;
+        // int c0s = 0;
+        // int c1s = 0;
+        // int c2s = 0;
+        // for (i=0; i<n; i++){
+        // if (arr[i]==0){
+        //     c0s+=1;
+        // }else if (arr[i]==1){
+        //     c1s+=1;
+        // }else if (arr[i]==2){
+        //     c2s+=1;
+        // }
+        // }
+        // int idx=0;
+        // for (i=0; i<c0s; i++){
+        //     arr[idx++] = 0;
         
-        }
-         for (i=0; i<c1s; i++){
-             arr[idx++] = 1;
+        // }
+        //  for (i=0; i<c1s; i++){
+        //      arr[idx++] = 1;
         
-        } for (i=0; i<c2s; i++){
-            arr[idx++] = 2;
+        // } for (i=0; i<c2s; i++){
+        //     arr[idx++] = 2;
         
+        // }
+        // one pass algorithm 
+        //Dutch National Flag Algorithm 
+        int low = 0; int mid=0; int high = n-1;
+        while (mid<=high){
+            if (arr[mid]==0){
+                swap(arr[mid], arr[low]);
+                low++;
+                mid++;
+            }
+            else if (arr[mid]==1){
+                mid++;
+            }
+            else {
+                swap(arr[mid], arr[high]);
+                high--;
+            }
         }
     }
 };
