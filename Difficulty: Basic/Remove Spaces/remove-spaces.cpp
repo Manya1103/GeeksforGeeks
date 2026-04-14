895fbbdd0dc80af8506ac6c731c23e1003caed68
Manya Garg <manya111203@gmail.com>
2026-04-14 22:43:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Remove Spaces/remove-spaces.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    string removeSpaces(string& s) {
+        // code here
+        int n = s.length();
+        string ans = "";
+        for(int i = 0; i < n; i++){
+            if(s[i] != ' '){
+                ans.push_back(s[i]);
+            }
+        }
+        return ans;
+    }
+};
